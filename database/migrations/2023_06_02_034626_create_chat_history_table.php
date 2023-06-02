5fa6b7d577be3a27249c685eb6bef2c31e8837f4
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2023-06-02 15:42:19 +0630

    first commit

---
+++ database/migrations/2023_06_02_034626_create_chat_history_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_history', function (Blueprint $table) {
+            $table->id();
+            $table->integer('conversation_id');
+            $table->text('human');
+            $table->text('translated_human_text');
+            $table->text('ai');
+            $table->text('tranaslated_ai_text');
+            $table->integer('prompt_tokens')->default(0);
+            $table->integer('completion_tokens')->default(0);
+            $table->integer('total_tokens')->default(0);
+            $table->dateTime('expire_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_history');
+    }
+};
