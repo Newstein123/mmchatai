0352645041871c47a8c40399c44fbacce5737ee2
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2023-06-25 15:20:27 +0630

    api update

---
+++ app/Http/Controllers/Api/ChatController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ChatHistoryResource;
+use App\Http\Resources\ChatNameResource;
+use App\Http\Resources\ChatResource;
+use App\Models\Chat;
+use App\Models\User;
+use App\Models\UserChat;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    public function index($id) {
+        $user_id = 1;
+        $chat = UserChat::where('conversation_id', $id)->where('user_id', $user_id)->get();
+        return response()->json($chat);
+        return ChatResource::collection($chat)->additional([
+            "success" => true,
+            "count" => $chat->count(),
+        ]);
+    }
+
+    public function chat_name() {
+        $user_id = 1;
+        $chat = UserChat::where('user_id', $user_id)->orderBy('id', 'desc')->get();
+        return response()->json($chat);
+        return ChatNameResource::collection($chat)->additional([
+            'success' => true,
+            "count" => $chat->count(),
+        ]); 
+    }
+}
